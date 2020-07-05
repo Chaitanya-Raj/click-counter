7703c3e117f82b2c91b9ff3bca2796435131a9a8
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-07-05 16:09:25 +0530

    testing out hub cli

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const changeBackground = () => {
+    document.querySelector(".container").style.background = `rgb(${Math.floor(
+      Math.random() * 100
+    )},${Math.floor(Math.random() * 100)},${Math.floor(Math.random() * 100)})`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      onClick={() => {
+        setCounter(counter + 1);
+        changeBackground();
+      }}
+    >
+      <div className="counter">{counter}</div>
     </div>
   );
 }
